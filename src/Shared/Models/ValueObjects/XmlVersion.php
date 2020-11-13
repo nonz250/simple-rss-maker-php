a0b9000c0efe469766ea6a88b6e1da6ad6d967f9
Nozomi Hosaka <nozomi.hosaka@smaregi.jp>
2020-11-14 01:11:02 +0900

    Add XmlVersion VO

---
+++ src/Shared/Models/ValueObjects/XmlVersion.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace SimpleRssMaker\Shared\Models\ValueObjects;
+
+use SimpleRssMaker\Foundation\ValueObjects\Enum;
+
+final class XmlVersion extends Enum
+{
+    public const VERSION_1 = '1.0';
+    public const VERSION_1_1 = '1.1';
+}
---
+++ tests/Shared/Models/ValueObjects/XmlVersionTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Shared\Models\ValueObjects;
+
+use InvalidArgumentException;
+use SimpleRssMaker\Shared\Models\ValueObjects\XmlVersion;
+use PHPUnit\Framework\TestCase;
+
+class XmlVersionTest extends TestCase
+{
+    public function testCreateVersion1()
+    {
+        $version = new XmlVersion(XmlVersion::VERSION_1);
+        $this->assertEquals(XmlVersion::VERSION_1, $version->value());
+        $this->assertEquals(XmlVersion::VERSION_1, (string)$version);
+    }
+
+    public function testCreateVersion1_1()
+    {
+        $version = new XmlVersion(XmlVersion::VERSION_1_1);
+        $this->assertEquals(XmlVersion::VERSION_1_1, $version->value());
+        $this->assertEquals(XmlVersion::VERSION_1_1, (string)$version);
+    }
+
+    public function testException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new XmlVersion('unknown');
+    }
+}
