88e517627324067a288b25b66c9f6a368bdc9fae
Nozomi Hosaka <nozomi.hosaka@smaregi.jp>
2020-11-14 03:37:49 +0900

    Add Description VO

---
+++ tests/Rss2/Models/ValueObjects/DescriptionTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Rss2\Models\ValueObjects;
+
+use SimpleRssMaker\Rss2\Models\ValueObjects\Description;
+use PHPUnit\Framework\TestCase;
+use Tests\TestHelper\StrTestHelper;
+
+class DescriptionTest extends TestCase
+{
+    public function test__construct()
+    {
+        $expected = StrTestHelper::createRandomStr();
+        $description = new Description($expected);
+        $this->assertInstanceOf(Description::class, $description);
+        $this->assertEquals($expected, (string)$description);
+        return $description;
+    }
+}
